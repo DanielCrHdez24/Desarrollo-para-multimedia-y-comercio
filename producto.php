3fbd45b5b26a644a3a46dcfe0531528d29f640fc
Daniel Cruz Hernandez <a22300104@unideh.edu.mx>
2025-05-18 13:41:50 -0600

    ....

--- producto.php
+++ producto.php
@@ -7,6 +7,7 @@
                     <h2>Camiseta personalizada</h2>
                     <p>Diseños únicos y exclusivos en varias tallas.</p>
                     <span class="precio">$169.90</span>
+                    <span class="existencia">97 pzas</span>
                 </div>
                 <div class="producto">
                     <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Taza%202.png?raw=true"
@@ -14,6 +15,7 @@
                     <h2>Taza con logo</h2>
                     <p>Cerámica de alta calidad con impresión duradera.</p>
                     <span class="precio">$109.90</span>
+                    <span class="existencia">62 pzas</span>
                 </div>
                 <div class="producto">
                     <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Gorra%202.png?raw=true"
@@ -21,6 +23,7 @@
                     <h2>Gorra bordada</h2>
                     <p>Perfecta para cualquier ocasión, ajustable y cómoda.</p>
                     <span class="precio">$149.90</span>
+                    <span class="existencia">83 pzas</span>
                 </div>
             </div>
         
