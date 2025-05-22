250f302d759280651df0113f6f3a8bafede95190
Daniel Cruz Hernandez <a22300104@unideh.edu.mx>
2025-05-22 17:44:21 -0600

    ....

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'inicio';
     <title>El Clan del Juego</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
