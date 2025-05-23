ebb3314b2bd4c95ecb09a877aec995cda96dcfc6
Daniel Cruz Hernandez <a22300104@unideh.edu.mx>
2025-05-23 00:02:02 -0600

    ....

--- producto.php
+++ producto.php
@@ -1,29 +1,31 @@
+<h1>¡Llévele, barato!</h1>
+<div class="container-fluid text-center">
+    <div class="catalogo row justify-content-center g-4">
+        <div class="producto col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex flex-column align-items-center">
+            <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Playera%202.png?raw=true"
+                alt="Camiseta personalizada" class="img-fluid rounded">
+            <h2>Camiseta personalizada</h2>
+            <p>Diseños únicos y exclusivos en varias tallas.</p>
+            <span class="precio">$169.90</span>
+            <span class="existencia">97 pzas</span>
+        </div>
 
-            <h1>¡Llévele, barato!</h1>
-            <div class="catalogo">
-                <div class="producto">
-                    <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Playera%202.png?raw=true"
-                        alt="Camiseta personalizada">
-                    <h2>Camiseta personalizada</h2>
-                    <p>Diseños únicos y exclusivos en varias tallas.</p>
-                    <span class="precio">$169.90</span>
-                    <span class="existencia">97 pzas</span>
-                </div>
-                <div class="producto">
-                    <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Taza%202.png?raw=true"
-                        alt="Taza con logo">
-                    <h2>Taza con logo</h2>
-                    <p>Cerámica de alta calidad con impresión duradera.</p>
-                    <span class="precio">$109.90</span>
-                    <span class="existencia">62 pzas</span>
-                </div>
-                <div class="producto">
-                    <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Gorra%202.png?raw=true"
-                        alt="Gorra bordada">
-                    <h2>Gorra bordada</h2>
-                    <p>Perfecta para cualquier ocasión, ajustable y cómoda.</p>
-                    <span class="precio">$149.90</span>
-                    <span class="existencia">83 pzas</span>
-                </div>
-            </div>
-        
+        <div class="producto col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex flex-column align-items-center">
+            <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Taza%202.png?raw=true"
+                alt="Taza con logo" class="img-fluid rounded">
+            <h2>Taza con logo</h2>
+            <p>Cerámica de alta calidad con impresión duradera.</p>
+            <span class="precio">$109.90</span>
+            <span class="existencia">62 pzas</span>
+        </div>
+
+        <div class="producto col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex flex-column align-items-center">
+            <img src="https://github.com/DanielCrHdez24/Imagenes_desarrollos/blob/main/MockUps/Gorra%202.png?raw=true"
+                alt="Gorra bordada" class="img-fluid rounded">
+            <h2>Gorra bordada</h2>
+            <p>Perfecta para cualquier ocasión, ajustable y cómoda.</p>
+            <span class="precio">$149.90</span>
+            <span class="existencia">83 pzas</span>
+        </div>
+    </div>
+</div>
